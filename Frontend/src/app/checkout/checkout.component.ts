440e1298fcb6808ad5341dacca30447c7441c9ae
bassantyasser043 <bassantmohamed043@gmail.com>
2023-01-05 12:43:06 +0200

    yarab

--- Frontend/src/app/checkout/checkout.component.ts
+++ Frontend/src/app/checkout/checkout.component.ts
@@ -11,6 +11,8 @@ import { CheckoutService } from "../services/checkout/checkout.service";
   styleUrls: ["./checkout.component.css"],
 })
 export class CheckoutComponent implements OnInit {
+  rExp : RegExp =/(^4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11}$)/g
+  validCard:boolean=false;
   constructor(
     private cartService: CartService,
     private authService: AuthService,
@@ -26,12 +28,23 @@ export class CheckoutComponent implements OnInit {
       customer: this.authService.getUserEmail(),
       books: this.cartService.getCheckoutProducts(),
     };
+    this.checkValidation();
+    if(this.validCard){
     console.log("Cart Products To Be Checkout => ", cartProducts);
     this.checkoutService.sendCheckoutProducts(cartProducts).subscribe(() => {
       console.log("We Checkout, Babe .. HeHe");
       this.cartService.clearCart();
       this.checked = true;
     });
+  }
+  else {
+    let ele = document.getElementById('Number-Valid');
+    if (ele) {
+      ele.style.display = 'block';
+      ele.style.color = 'red';
+      ele.style.paddingLeft = '180px';
+    }
+  }
   }
 
   getInformation() {
@@ -56,5 +69,11 @@ export class CheckoutComponent implements OnInit {
 
   backHome() {
     this.router.navigateByUrl("home", { state: { logged: true } });
+  }
+  checkValidation(){
+    let ele = document.getElementById('Number-Valid');
+    if (ele) ele.style.display = 'none';
+    this.validCard=this.rExp.test((<HTMLInputElement>document.getElementById("ccnum")).value);
+    console.log(this.validCard);
   }
 }
